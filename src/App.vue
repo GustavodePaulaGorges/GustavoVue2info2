a102de01b73fd68dbf4d85237ab4eeb688223136
Gustavo P. Gorges <99231805+GustavodePaulaGorges@users.noreply.github.com>
2022-04-25 20:41:12 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import ComponenteCabecalho from "@/components/ComponenteCabecalho.vue";
+import ComponenteRodape from "@/components/ComponenteRodape.vue";
+import ComponentPrincipal from "@/components/ComponentPrincipal.vue";
+export default {
+  components: { MenuLateral, ComponenteCabecalho, ComponenteRodape, ComponentPrincipal, },
+};
+</script>
+<template>
+  <MenuLateral />
+  <ComponenteCabecalho />
+  <ComponenteRodape/>
+  <ComponentPrincipal/>
+</template>
+<style></style>
---
+++ src/components/ComponentPrincipal.vue
@@ -0,0 +1,19 @@
+<script>
+import NovoBloco from "./NovoBloco.vue";
+export default {
+    components: { NovoBloco }
+};
+</script>
+
+<template>
+    <main id="content">Principal
+        <NovoBloco />
+        <NovoBloco />
+        <NovoBloco />
+        <NovoBloco />
+        <NovoBloco />
+        <NovoBloco />
+    </main>
+</template>
+
+<style></style>
